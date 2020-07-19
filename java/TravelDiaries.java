46f7d3d61d647e0fb7dcfa01e7598af0e74fb0cd
Rohitash Goyal <32011743+Rohitash28@users.noreply.github.com>
2020-07-19 18:47:12 +0530

    anotherone

---
+++ java/TravelDiaries.java
@@ -0,0 +1,36 @@
+
+/*
+ * WAP to create a class traveldiaries to create  a menu driven program fort
+ * asking a user for booking the package. Initially ask the customer details like 
+ * name adhar id and ph num of customer than show the various packages for 
+ * diffrent state.If user select any state than ask him for the hotel that is 
+ * available for booking with available meal plan or sight seeing plan. Ask to
+ * CUSTOMER is he or she wish to see in details includedin meal or sight seeing
+ * now ask for the confirmation for how many days a customer want to book the
+ * hotel with meal or with sight seeing.Finally show the bill to the customer.
+ * 
+ */
+import java.util.*;
+public class TravelDiaries {
+    public static void main(String s[]){
+        Scanner sc = new Scanner(System.in);
+        TravelDiaries t= new TravelDiaries();
+        int ad_id,ph_no;
+        System.out.println("enter the name");
+        String name st.nextline();
+        System.out.println("Enetr the id ");
+        int id= st.nextInt();
+        System.out.println("enter the Govt Id");
+        int ad_id=nextInt();
+        System.out.println("1. For punjab is  10000");
+        System.out.println("2. For Haryana is 8000");
+        System.out.println("3. For Rajasthan is 12000");
+        System.out.println("4. For Himachal is 15000");
+        int c= st.nextInt();
+        switch(c)
+        {
+            case 1:
+                    System.out.println("Welcome to  Punjab ");
+                    System.out.println
+    }
+}
