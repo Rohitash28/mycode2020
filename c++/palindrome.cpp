fea8d565bf0a3b1f83d7f6a006162ae4a320451e
Rohitash Goyal <32011743+Rohitash28@users.noreply.github.com>
2020-08-10 10:11:59 +0530

    10aug

---
+++ c++/palindrome.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int n, num, digit; 
+	 int rev = 0;
+
+     cout << "Enter number to check: ";
+     cin >> num;
+
+     n = num;
+
+     do
+     {
+         digit = num % 10;
+         rev = (rev * 10) + digit;
+         num = num / 10;
+     } while (num != 0);
+
+     cout << " The reverse of the number is: " << rev << endl;
+
+     if (n == rev)
+         cout << " The number is a palindrome.";
+     else
+         cout << " The number is not a palindrome.";
+
+    return 0;
+}
